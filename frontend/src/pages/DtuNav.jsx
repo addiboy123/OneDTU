7eebcdbff1c2eadaac28ba9885bba0868af47d08
aditya <adityabibhas.sahu@gmail.com>
2025-10-11 11:10:17 +0530

    cicd test

--- frontend/src/pages/DtuNav.jsx
+++ frontend/src/pages/DtuNav.jsx
@@ -129,7 +129,7 @@ export default function DtuNav() {
   return (
     <div
       className="min-h-screen flex flex-col"
-      style={{ backgroundColor: "#FFFFFF" }}
+      style={{ backgroundColor: "#ff7300ff" }}
     >
       {/* Navbar */}
       <Navbar />
